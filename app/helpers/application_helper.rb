ffc30eee23961b584084cb07a9a405a6343fb916
philipfweiss <weissp68@stanford.edu>
2016-10-23 10:28:37 -0700

    add basic page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
    def full_title(page_title='')
-     base_title = "Application Base"
+     base_title = "Secret Shlomo"
      if page_title.empty?
        base_title
      else
