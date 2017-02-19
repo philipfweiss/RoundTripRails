eaefedc8ca94ec73f647622787220b7d77214f26
philipfweiss <weissp68@stanford.edu>
2017-02-19 03:17:48 -0800

    fix crash

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,6 @@ class StaticPagesController < ApplicationController
 
   def entercities
     @id = params[:id].to_i
-    @user = User.new
     redirect_to(root_path) if(@id > 15.to_i || @id < 0)
     flash.now[:success] = "Please Enter #{@id} cities"
 
